e8cb0bfd1ec35a0c06f2803cb76a64f4f2bc2f0b
barrie <barrie@reboot-robot.net>
2016-07-10 22:50:56 +0100

    CPU  flags working as an int or individual flags

--- cpu.py
+++ cpu.py
@@ -1,25 +1,49 @@
-from pprint import pprint
-
 import numpy as np
 
 MOS_6510_RAM_SIZE = 0xFFFF
 
 
 class StatusRegister:
+    """MOS P Register, individual flags as booleans"""
+
     def __init__(self):
         # http://wiki.nesdev.com/w/index.php/CPU_status_flag_behavior
-        self.carry = False  # Carry: 1 if last addition or shift resulted in a carry,
+
+        self.carry = 0  # Carry: 1 if last addition or shift resulted in a carry,
         # or if last subtraction resulted in no borrow
-        self.zero = False  # Zero: 1 if last operation resulted in a 0 value
-        self.interrupt = True  # Interrupt: Interrupt inhibit - interrupts are disabled if this is true!
-        self.decimal = False  # Decimal: 1 to make ADC and SBC use binary-coded decimal arithmetic
-        self.unused1 = False  # No effect, used by the stack copy
-        self.unused2 = True  # No effect, used by the stack copy
-        self.overflow = False  # Overflow: 1 if last ADC or SBC resulted in signed overflow
-        self.negative = False  # Negative: Set to bit 7 of the last operation
+        self.zero = 0  # Zero: 1 if last operation resulted in a 0 value
+        self.interrupt = 1  # Interrupt: Interrupt inhibit - interrupts are disabled if this is true!
+        self.decimal = 0  # Decimal: 1 to make ADC and SBC use binary-coded decimal arithmetic
+        self.unused1 = 0  # No effect, used by the stack copy
+        self.unused2 = 1  # No effect, used by the stack copy
+        self.overflow = 0  # Overflow: 1 if last ADC or SBC resulted in signed overflow
+        self.negative = 0  # Negative: Set to bit 7 of the last operation
+
+    @property
+    def flags(self):
+        """Packs all the status flags into a uint8 """
+        return np.packbits([self.carry,
+                            self.zero,
+                            self.interrupt,
+                            self.decimal,
+                            self.unused1,
+                            self.unused2,
+                            self.overflow,
+                            self.negative])[0]  # packbits returns an array but we only want the single element
+
+    @flags.setter
+    def flags(self, P):
+        """Unpacks status flags from an int """
+        self.carry, self.zero, self.interrupt, self.decimal, self.unused1, self.unused2, self.overflow, self.negative = np.unpackbits(
+            np.asarray(P, dtype="uint8"))
+
+    def __str__(self):
+        return "Flags:\tCZIDuuON\n\t\t{:08b}".format(self.flags)
 
 
 class Cpu:
+    """ Implements MOS 65XX series of CPUs, currently supports only 6510"""
+
     def __init__(self):
         self.model = (6, 5, 1, 0)
         self.ram = np.zeros(MOS_6510_RAM_SIZE, dtype=np.int8)
@@ -35,7 +59,8 @@ class Cpu:
 def main():
     proc = Cpu()
     print("CPU Model: {0}".format(proc.model))
-    print("CPU RAM Size: {0} ({1})".format(proc.ram.size, hex(proc.ram.size)))
+    print("CPU RAM Size: {0} ({0:X})".format(proc.ram.size))
+    print(proc.P)
 
 
 if __name__ == "__main__":
--- unittests/test_cpu.py
+++ unittests/test_cpu.py
@@ -2,15 +2,17 @@ import unittest
 from unittest import TestCase
 
 from cpu import Cpu as processor
-test_cpu = processor()
+
 
 class TestCpu(TestCase):
     def test_6510_ram_size(self):
+        test_cpu = processor()
         self.assertEqual(test_cpu.ram.size, 0xFFFF)
 
 
 class TestStatusRegister(TestCase):
     def test_register_initialisation(self):
+        test_cpu = processor()
         self.assertTrue(test_cpu.P.interrupt)
         self.assertTrue(test_cpu.P.unused2)
 
@@ -19,3 +21,16 @@ class TestStatusRegister(TestCase):
         self.assertFalse(test_cpu.P.unused1)
         self.assertFalse(test_cpu.P.overflow)
         self.assertFalse(test_cpu.P.negative)
+
+    def test_get_set_flags(self):
+        test_cpu = processor()
+        test_cpu.P.flags = 0xFF
+        self.assertTrue(test_cpu.P.interrupt)
+        self.assertTrue(test_cpu.P.unused2)
+        self.assertTrue(test_cpu.P.carry)
+        self.assertTrue(test_cpu.P.zero)
+        self.assertTrue(test_cpu.P.unused1)
+        self.assertTrue(test_cpu.P.overflow)
+        self.assertTrue(test_cpu.P.negative)
+
+        self.assertEqual(test_cpu.P.flags, 0xFF)
