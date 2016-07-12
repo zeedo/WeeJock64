2746f0c06daef1ead1203c7bd177b85e5940d750
barrie <barrie@reboot-robot.net>
2016-07-12 23:03:30 +0100

    Added opcode_to_named tuple

--- opcode_table.py
+++ opcode_table.py
@@ -1,4 +1,5 @@
 import csv
+from collections import namedtuple
 
 
 class opcodes(object):
@@ -11,20 +12,26 @@ class opcodes(object):
         with open(opcode_file, 'r') as csvfile:
             csv_data = csv.reader(csvfile, delimiter=';', )
             for rows in csv_data:
-                self.op_table[int(rows[0], 16)] = rows[1:]  # first row is two characters in hex format, pares the rest as strings
+                self.op_table[int(rows[0], 16)] = self.opcode_to_named_tuple(
+                    rows[1:])  # first row is two characters in hex format, parse the rest as strings
 
     def lookup_hex_code(self, hex_code):
         try:
-            return self.op_table[hex_code][0]
+            return self.op_table[hex_code].mnemonic
         except KeyError as e:
             raise Exception("Instruction not found: 0x{0:002X}".format(hex_code))
+
+    @staticmethod
+    def opcode_to_named_tuple(opcode):
+        Opcode_tuple = namedtuple('Opcode_tuple', ['mnemonic', 'mode', 'cycles'])
+        return Opcode_tuple(*opcode)
 
 
 def main():
     op = opcodes()
 
     for hex_code, opcode in op.op_table.items():
-        print("0x{0:002X}\t{1}\t\t{2: <8}{3}".format(hex_code, opcode[0], opcode[1], opcode[2], ))
+        print("0x{0:002X}\t{1}\t\t{2: <8}{3}".format(hex_code, opcode.mnemonic, opcode.mode, opcode.cycles, ))
 
 
 if __name__ == "__main__":
